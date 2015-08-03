02fbf0fe81bf99f73f23d49bcc4496be8bfa17ee
Dave Wasmer <davewasmer@gmail.com>
2015-08-03 08:44:19 -0400

    add meta-for function and test coverage

--- test/index.js
+++ test/index.js
@@ -1,8 +1,21 @@
 import assert from 'assert';
-import metaprop from '../lib';
+import metaFor from '../lib';
 
-describe('metaprop', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+describe('meta-for', function() {
+  it('should return the same empty object for any objects without a meta.', function() {
+    assert(metaFor({}) === metaFor({}));
+  });
+  it('should return the unique meta object for an object whose meta has been written to', function() {
+    let obj = {};
+    let meta = metaFor(obj, true);
+    meta.foobar = true;
+    assert(metaFor(obj).foobar);
+  });
+  it('should not persist writes back unless the writable option passed in', function() {
+    let obj = {};
+    let meta = metaFor(obj);
+    assert.throws(function() {
+      meta.foobar = true;
+    });
   });
 });
