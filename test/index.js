b1d9e6a42fa9347aba682ad7b0cd13822b481210
Dave Wasmer <davewasmer@gmail.com>
2015-08-03 10:04:25 -0400

    update tests for readonly flag and getmeta + setmeta

--- test/index.js
+++ test/index.js
@@ -1,21 +1,38 @@
 import assert from 'assert';
-import metaFor from '../lib';
+import metaFor, { getMeta, setMeta } from '../lib';
 
-describe('meta-for', function() {
-  it('should return the same empty object for any objects without a meta.', function() {
-    assert(metaFor({}) === metaFor({}));
+describe('metaFor', function() {
+  it('should return the same empty object for any objects without a meta when readonly is true.', function() {
+    assert(metaFor({}, true) === metaFor({}, true));
   });
   it('should return the unique meta object for an object whose meta has been written to', function() {
     let obj = {};
-    let meta = metaFor(obj, true);
+    let meta = metaFor(obj);
     meta.foobar = true;
     assert(metaFor(obj).foobar);
   });
-  it('should not persist writes back unless the writable option passed in', function() {
+  it('should not allow writes back if the readonly option passed in', function() {
     let obj = {};
-    let meta = metaFor(obj);
+    let meta = metaFor(obj, true);
     assert.throws(function() {
       meta.foobar = true;
     });
+  });
+});
+
+describe('setMeta', function() {
+  it('should set a meta property', function() {
+    let obj = {};
+    let meta = metaFor(obj);
+    setMeta(obj, 'foobar', true);
+    assert(meta.foobar);
+  });
+});
+
+describe('getMeta', function() {
+  it('should set a meta property', function() {
+    let obj = {};
+    setMeta(obj, 'foobar', true);
+    assert(getMeta(obj, 'foobar'));
   });
 });
